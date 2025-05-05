939e0fa4a900878f64160d7ffb5980d88fa0fad5
ajb601 <90455297+ajb601@users.noreply.github.com>
2025-05-05 12:33:30 -0400

    Limit response to top 25 QBs by passing yards

--- netlify/functions/getPlayerStats.js
+++ netlify/functions/getPlayerStats.js
@@ -4,17 +4,24 @@ exports.handler = async function(event, context) {
   
     try {
       const url = `https://api.collegefootballdata.com/stats/player/season?year=2023&position=QB`;
+  
       const response = await fetch(url, {
         headers: {
           Authorization: `Bearer ${apiKey}`
         }
       });
   
-      const data = await response.json();
+      const fullData = await response.json();
+  
+      // Filter to only top 25 players with most passing yards
+      const topQBs = fullData
+        .filter(p => p.statType === 'passingYards')
+        .sort((a, b) => Number(b.stat) - Number(a.stat))
+        .slice(0, 25);
   
       return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify(topQBs)
       };
     } catch (err) {
       return {
